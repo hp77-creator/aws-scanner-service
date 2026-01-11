0fba57ed59370a9863da92742e7be658d69b8b79
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2026-01-11 22:04:54 +0530

    Setup spring-boot project

---
+++ src/main/java/io/hp77creator/github/awsscannerservice/AwsScannerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.hp77creator.github.awsscannerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsScannerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsScannerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hp77creator/github/awsscannerservice/AwsScannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hp77creator.github.awsscannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsScannerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
